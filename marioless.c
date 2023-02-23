f2953382e5d434398e242854b01374d99bdffc72
bradikk <124538028+bradikk@users.noreply.github.com>
2023-02-23 13:31:33 -0800

    Add files via upload

---
+++ marioless.c
@@ -0,0 +1,36 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt user for "height"
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    // Inlude only numbers 1 - 8
+    while (height < 1 || height > 8);
+
+    // Draw half pyramid
+    // Create row
+    for (int row = 0; row < height; row++)
+    {
+        // Create column
+        for (int column = 0; column < height; column++)
+        {
+            if (height > row + column + 1)
+            {
+                // Print spaces
+                printf(" ");
+            }
+            else
+            {
+                // Print hashes
+                printf("#");
+            }
+        }
+        // Print new line
+        printf("\n");
+    }
+}
